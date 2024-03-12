e59f5ad83f842001e814049766fdb1dc3fb2b864
arT2 <60965610+etrusci-org@users.noreply.github.com>
2024-03-12 23:12:19 +0100

    More + markdown

--- bake.py
+++ bake.py
@@ -6,8 +6,9 @@ import json
 
 
 SOURCE_FILE = './_src.json'
-OUT_FILE_JSON = './quotes.json'
-OUT_FILE_JSON_MIN = './quotes.min.json'
+OUT_FILE_JSON = './json/quotes.json'
+OUT_FILE_JSON_MIN = './json/quotes.min.json'
+OUT_FILE_MARKDOWN = './markdown/quotes.md'
 
 
 
@@ -15,16 +16,31 @@ OUT_FILE_JSON_MIN = './quotes.min.json'
 def bake():
     with open(SOURCE_FILE, 'r') as src_f, \
          open(OUT_FILE_JSON, 'w') as out_f_json, \
-         open(OUT_FILE_JSON_MIN, 'w') as out_f_json_min:
+         open(OUT_FILE_JSON_MIN, 'w') as out_f_json_min, \
+         open(OUT_FILE_MARKDOWN, 'w') as out_f_md:
 
         src = json.load(src_f)
 
-        print('_src.json > quotes.json')
+        # src > json
+        print(f'{SOURCE_FILE} > {OUT_FILE_JSON}')
         json.dump(src, out_f_json, ensure_ascii=True, indent=4)
 
-        print('_src.json > quotes.min.json')
+        # src > json.min
+        print(f'{SOURCE_FILE} > {OUT_FILE_JSON_MIN}')
         json.dump(src, out_f_json_min, ensure_ascii=True)
 
+        # src > quotes.md
+        print(f'{SOURCE_FILE} > {OUT_FILE_MARKDOWN}')
+        dump = {}
+        for v in src:
+            if not v['author'] in dump.keys():
+                dump[v['author']] = []
+            dump[v['author']].append(v['text'])
+        out_f_md.write(f'# Quotes\n')
+        for author, quotes in dump.items():
+            out_f_md.write(f'\n\n## {author}\n\n')
+            for v in quotes:
+                out_f_md.write(f'- {v}\n')
 
 
 
