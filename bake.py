c51c69c25526b35d34ec4d6fd60d55a8eb9ef721
arT2 <60965610+etrusci-org@users.noreply.github.com>
2024-03-17 10:05:37 +0100

    Minified authors

--- bake.py
+++ bake.py
@@ -10,6 +10,7 @@ OUT_FILE_JSON = './json/quotes.json'
 OUT_FILE_JSON_MIN = './json/quotes.min.json'
 OUT_FILE_MARKDOWN = './markdown/quotes.md'
 OUT_FILE_AUTHORS_JSON = './authors.json'
+OUT_FILE_AUTHORS_JSON_MIN = './authors.min.json'
 OUT_FILE_AUTHORS_TXT = './authors.txt'
 
 
@@ -20,8 +21,9 @@ def bake():
          open(OUT_FILE_JSON, 'w') as out_f_json, \
          open(OUT_FILE_JSON_MIN, 'w') as out_f_json_min, \
          open(OUT_FILE_MARKDOWN, 'w') as out_f_md, \
-         open(OUT_FILE_AUTHORS_TXT, 'w') as out_f_authors_txt, \
-         open(OUT_FILE_AUTHORS_JSON, 'w') as out_f_authors_json:
+         open(OUT_FILE_AUTHORS_JSON, 'w') as out_f_authors_json, \
+         open(OUT_FILE_AUTHORS_JSON_MIN, 'w') as out_f_authors_json_min, \
+         open(OUT_FILE_AUTHORS_TXT, 'w') as out_f_authors_txt:
 
         src = json.load(src_f)
 
@@ -30,16 +32,13 @@ def bake():
             if not v['author'] in authors:
                 authors.append(v['author'])
 
-        # src > json
-        print(f'{SOURCE_FILE} > {OUT_FILE_JSON}')
+        # src > quotes.json
         json.dump(src, out_f_json, indent=4)
 
-        # src > json.min
-        print(f'{SOURCE_FILE} > {OUT_FILE_JSON_MIN}')
+        # src > quotes.min.json
         json.dump(src, out_f_json_min)
 
-        # src > markdown
-        print(f'{SOURCE_FILE} > {OUT_FILE_MARKDOWN}')
+        # src > quotes.md
         dump = {}
         for v in src:
             if not v['author'] in dump.keys():
@@ -52,12 +51,14 @@ def bake():
                 out_f_md.write(f'- {v}\n')
 
         # src > authors.txt
-        print(f'{SOURCE_FILE} > {OUT_FILE_AUTHORS_TXT}')
         out_f_authors_txt.write('\n'.join(sorted(authors)))
 
         # src > authors.json
-        print(f'{SOURCE_FILE} > {OUT_FILE_AUTHORS_JSON}')
         json.dump(authors, out_f_authors_json, indent=4)
+
+        # src > authors.min.json
+        json.dump(authors, out_f_authors_json_min)
+
 
 
 
