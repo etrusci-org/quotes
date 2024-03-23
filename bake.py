7b18bcd8af519a683aec87e1aea7884c710d0e7a
arT2 <60965610+etrusci-org@users.noreply.github.com>
2024-03-23 12:15:41 +0100

    CF

--- bake.py
+++ bake.py
@@ -5,10 +5,13 @@ import json
 
 
 
-SOURCE_FILE = './_src.json'
+SRC_FILE = './_src.json'
+
 OUT_FILE_JSON = './json/quotes.json'
 OUT_FILE_JSON_MIN = './json/quotes.min.json'
-OUT_FILE_MARKDOWN = './markdown/quotes.md'
+
+OUT_FILE_MD = './markdown/quotes.md'
+
 OUT_FILE_AUTHORS_JSON = './authors.json'
 OUT_FILE_AUTHORS_JSON_MIN = './authors.min.json'
 OUT_FILE_AUTHORS_TXT = './authors.txt'
@@ -16,58 +19,53 @@ OUT_FILE_AUTHORS_TXT = './authors.txt'
 
 
 
-def bake():
-    with open(SOURCE_FILE, 'r') as src_f, \
-         open(OUT_FILE_JSON, 'w') as out_f_json, \
-         open(OUT_FILE_JSON_MIN, 'w') as out_f_json_min, \
-         open(OUT_FILE_MARKDOWN, 'w') as out_f_md, \
-         open(OUT_FILE_AUTHORS_TXT, 'w') as out_f_authors_txt, \
-         open(OUT_FILE_AUTHORS_JSON, 'w') as out_f_authors_json, \
-         open(OUT_FILE_AUTHORS_JSON_MIN, 'w') as out_f_authors_json_min:
+QUOTES = []
+AUTHORS = []
 
-        print(f'< {SOURCE_FILE}')
-        src = json.load(src_f)
+with open(SRC_FILE, 'r') as f:
+    QUOTES = json.load(f)
 
-        authors = []
-        for v in src:
-            if not v['author'] in authors:
-                authors.append(v['author'])
+for v in QUOTES:
+    if not v['author'] in AUTHORS:
+        AUTHORS.append(v['author'])
 
-        # src > quotes.json
-        print(f'> {OUT_FILE_JSON}')
-        json.dump(src, out_f_json, indent=4)
 
-        # src > quotes.min.json
-        print(f'> {OUT_FILE_JSON_MIN}')
-        json.dump(src, out_f_json_min)
 
-        # src > quotes.md
-        print(f'> {OUT_FILE_MARKDOWN}')
+
+def main():
+    bake_json()
+    bake_markdown()
+    bake_authors()
+
+
+def bake_json():
+    with open(OUT_FILE_JSON, 'w') as f1, open(OUT_FILE_JSON_MIN, 'w') as f2:
+        json.dump(QUOTES, f1, indent=4)
+        json.dump(QUOTES, f2)
+
+
+def bake_markdown():
+    with open(OUT_FILE_MD, 'w') as f1:
         dump = {}
-        for v in src:
+        for v in QUOTES:
             if not v['author'] in dump.keys():
                 dump[v['author']] = []
             dump[v['author']].append(v['text'])
-        out_f_md.write(f'# Quotes\n')
+        f1.write(f'# Quotes\n')
         for author, quotes in dump.items():
-            out_f_md.write(f'\n\n## {author}\n\n')
+            f1.write(f'\n\n## {author}\n\n')
             for v in quotes:
-                out_f_md.write(f'- {v}\n')
+                f1.write(f'- {v}\n')
 
-        # src > authors.txt
-        print(f'> {OUT_FILE_AUTHORS_TXT}')
-        out_f_authors_txt.write('\n'.join(sorted(authors)))
 
-        # src > authors.json
-        print(f'> {OUT_FILE_AUTHORS_JSON}')
-        json.dump(authors, out_f_authors_json, indent=4)
-
-        # src > authors.min.json
-        print(f'> {OUT_FILE_AUTHORS_JSON_MIN}')
-        json.dump(authors, out_f_authors_json_min)
+def bake_authors():
+    with open(OUT_FILE_AUTHORS_TXT, 'w') as f1, open(OUT_FILE_AUTHORS_JSON, 'w') as f2, open(OUT_FILE_AUTHORS_JSON_MIN, 'w') as f3:
+        f1.write('\n'.join(sorted(AUTHORS)))
+        json.dump(AUTHORS, f2, indent=4)
+        json.dump(AUTHORS, f3)
 
 
 
 
 if __name__ == '__main__':
-    bake()
+    main()
